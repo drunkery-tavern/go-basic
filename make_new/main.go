e39e885efa003b7202f939a01d4245e0b6267b9a
Einson <1519695805@qq.com>
2021-08-11 18:10:09 +0800

    make and new

--- make_new/main.go
+++ make_new/main.go
@@ -8,7 +8,17 @@ func main() {
 	log.Println(*i) // 0
 
 	m := new(map[int]int)
-	log.Println(*m) //map[]
-	(*m)[1] = 1     // panic: assignment to entry in nil map
+	log.Println(*m)        //map[]
+	log.Println(*m == nil) //true
+	//(*m)[1] = 1     // panic: assignment to entry in nil map
 
+	slice := new([]int)
+	log.Println(*slice)        //[]
+	log.Println(*slice == nil) //true
+
+	m2 := make(map[int]int, 3)
+	log.Println(m2)        //map[]
+	log.Println(m2 == nil) //false
+	m2[1] = 1
+	log.Println(m2[1]) //1
 }
