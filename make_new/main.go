e2df721ff22b439384cdc3301f655ef542a0f607
Einson <1519695805@qq.com>
2021-08-11 18:58:21 +0800

    make and new

--- make_new/main.go
+++ make_new/main.go
@@ -20,5 +20,24 @@ func main() {
 	log.Println(m2)        //map[]
 	log.Println(m2 == nil) //false
 	m2[1] = 1
-	log.Println(m2[1]) //1
+	log.Println(m2) //map[1:1]
+	modifyMap(m2)
+	log.Println(m2) //map[1:2]
+
+	channel := make(chan string, 1)
+	log.Println(channel)        //0xc00004e1e0
+	log.Println(channel == nil) //false
+	channel <- "string"
+	log.Println(<-channel) //string
+	modifyChannel(channel)
+	log.Println(<-channel) //new string
+
+}
+
+func modifyMap(m map[int]int) {
+	m[1] = 2
+}
+
+func modifyChannel(c chan string) {
+	c <- "new string"
 }
