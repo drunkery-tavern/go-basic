efb97a98ef72df7967c9dddf1b9e36e63528ce42
Einson <1519695805@qq.com>
2021-09-24 18:03:46 +0800

    fix protoc.bat

--- go-grpc/protoc.bat
+++ go-grpc/protoc.bat
@@ -1,5 +1,7 @@
-bin\protoc --go_out=./api/v1/ --go_opt=paths=source_relative --go-grpc_out=./api/v1/ --go-grpc_opt=paths=source_relative  --proto_path=./api/v1/ ./api/v1/api.proto
-
-for /f %%f in ('dir /b \api\v1*.proto' ) do (
+@echo off
+set BIN_DIR=bin
+del /f /s /q .\api\v1\*.go
+for /f %%f in ('dir /b .\api\v1\*.proto' ) do (
     echo %%~nf
+    %BIN_DIR%\protoc --go_out=./api/v1/ --go_opt=paths=source_relative --go-grpc_out=./api/v1/ --go-grpc_opt=paths=source_relative --proto_path=./api/v1/ ./api/v1/%%~nf.proto
 )
